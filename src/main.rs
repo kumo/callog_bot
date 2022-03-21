dd28f36b246cbad3ba2096b6f3fa48c36602209a
Robert Clarke <kumo@kumo.it>
2022-03-21 21:41:58 +0100

    Adding struct and helper functions

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ use scraper::{Html, Selector};
 use chrono::NaiveDateTime;
 use chrono::Utc;
 
-async fn list_calls() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Eq, PartialEq)]
+#[derive(Debug)]
+struct PhoneCall {
+  who: String,
+  when: NaiveDateTime
+}
+
+async fn get_calls() -> Result<Vec<PhoneCall>, Box<dyn std::error::Error>> {
+  let mut phone_calls:Vec<PhoneCall> = Vec::new();
+
   let resp = reqwest::get("http://192.168.1.1/callLog.lp").await?.text().await?;
 
   let document = Html::parse_document(&resp);
@@ -25,16 +34,41 @@ async fn list_calls() -> Result<(), Box<dyn std::error::Error>> {
       println!("Parsed date and time is: {}", date_time);
       let diff = Utc::now().naive_utc() - date_time;
       println!("Phone call was {} minutes ago", diff.num_minutes());
-    }
 
+      let phone_call: PhoneCall = PhoneCall{who: tds[1].to_string(), when: date_time};
+      phone_calls.push(phone_call);
+    }
   }
+
+  return Ok(phone_calls);
+}
+
+async fn list_all_calls() -> Result<(), Box<dyn std::error::Error>> {
+  let phone_calls:Vec<PhoneCall> = get_calls().await?;
+
+  if phone_calls.is_empty() {
+    println!("There are no phone calls in memory.")
+  } else {
+    println!("There are {} phone calls.", phone_calls.len());
+  }
+
+  Ok(())
+}
+
+async fn list_recent_calls() -> Result<(), Box<dyn std::error::Error>> {
+  let recent_phone_calls:Vec<PhoneCall> = get_calls().await?
+    .into_iter()
+    .filter(|phone_call| Utc::now().naive_utc().signed_duration_since(phone_call.when).num_days() < 1)
+    .collect();
+
+  println!("There are {} recent phone calls.", recent_phone_calls.len());
 
   Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  list_calls().await?;
+  list_all_calls().await?;
 
   Ok(())
 }
