2e848b6e604afdd70cd0bdee847c74ffe405d072
Robert Clarke <kumo@kumo.it>
2022-03-21 18:43:28 +0100

    Adding some comments

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let selector = Selector::parse(r#"table.edittable > tbody > tr"#).unwrap();
 
   // iterate over elements matching our selector
+  // we skip the first two, because the header is in the body
+  // and the second line draws the border
   for row in document.select(&selector).skip(2) {
-    // grab the headline text and place into a vector
+    // grab the table cells and place into a vector
     let tds = row.text().collect::<Vec<_>>();
 
-    // let output_tds = tds.drain_filter(|td| td.len() > 1).collect::<Vec<_>>();
+    // the table contains rows with no elements, due to bad spacing
     if tds.len() > 7 && tds[5] == "Ingresso" {
-      println!("{:?}", tds[1]);
-      println!("{:?}", tds[7]);
+      println!("{:?}", tds[1]); // phone number 
+      println!("{:?}", tds[7]); // raw date
+
+      // TODO: I am not sure that the phone number is in UTC
       let date_time = NaiveDateTime::parse_from_str(tds[7], "%H:%M:%S - %d:%m:%Y")?;
       println!("Parsed date and time is: {}", date_time);
       let diff = Utc::now().naive_utc() - date_time;
-      println!("Total time taken to run is {}", diff.num_minutes());
+      println!("Phone call was {} minutes ago", diff.num_minutes());
     }
 
   }
